41181db18de3c98d75b42fad99743172f170d889
octo <dlqhdgkr456@gmail.com>
2021-10-06 16:51:49 +0900

    First steps with Typescript

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
-console.log("Hello");
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const name = "hi", age = 26, gender = "male";
+// ? : 해당 parameter가 선택적인 것으로 설정
+const sayHi = (name, age, gender) => {
+    console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+sayHi(name, age);
 //# sourceMappingURL=index.js.map
--- index.ts
+++ index.ts
@@ -1,1 +1,13 @@
-console.log("Hello");
+const name = "hi",
+  age = 26,
+  gender = "male";
+
+// ? : 해당 parameter가 선택적인 것으로 설정
+const sayHi = (name, age, gender?) => {
+  console.log(`Hello ${name}, you are ${age}, you are a ${gender}`);
+};
+
+sayHi(name, age, gender);
+
+// typescript 규칙 중 하나 -> 이 파일이 모듈이 된다는 것을 이해할 수 있도록
+export {};
