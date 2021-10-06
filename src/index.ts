82f58f185c619d9f765cdf80f71a7e9612a6e943
octo <dlqhdgkr456@gmail.com>
2021-10-06 17:24:35 +0900

    0.7 Classes on Typescript part One

--- src/index.ts
+++ src/index.ts
@@ -24,5 +24,26 @@ const sayHiTwo = (person): string => {
 console.log(sayHi("World", 101, "male"));
 console.log(sayHiTwo(person));
 
+
+// interface의 경우 javascript로 컴파일 되지 않음
+// 그러나 아주 가끔 javascript에 interface를 넣고 싶은 경우 class를 사용
+// public vs private : class 밖에서 해당 변수를 불을 수 있으면 public, class 내부에서만 불러올 수 있으면 private
+class humanClass {
+  public name: string;
+  public age: number;
+  public gender: string;
+
+  // 생성자 method - 클래스로부터 object를 만들 때마다 호출됨
+  constructor(name: string, age: number, gender: string) {
+    this.name = name;
+    this.age = age;
+    this.gender = gender;
+  }
+}
+
+const personTwo = new humanClass("Hello", 333, "male");
+console.log(sayHiTwo(personTwo));
+
+
 // typescript 규칙 중 하나 -> 이 파일이 모듈이 된다는 것을 이해할 수 있도록
 export {};
