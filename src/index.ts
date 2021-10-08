74dd37cc064ee9619da22543dd7349e4b8e3ad4b
octo <dlqhdgkr456@gmail.com>
2021-10-08 22:15:57 +0900

    0.11 Validating Block Structure

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,6 @@
 import * as CryptoJS from 'crypto-js';
 
 class Block {
-  public index: number;
-  public hash: string;
-  public previousHash: string;
-  public data: string;
-  public timestamp: number;
-
   // static method : method가 Block class 안에 있고, class가 생성되지 않았어도  호출할 수 있는 method 즉, Block class 안에서 항상 사용가능한 method
   static calculateBlockHash = (
     index: number, 
@@ -15,6 +9,19 @@ class Block {
     data: string
   ): string => CryptoJS.SHA256(index + previousHash + timestamp + data).toString();
   
+
+  static validateStructure = (aBlock: Block): boolean => 
+    typeof aBlock.index === "number" && 
+    typeof aBlock.hash === "string" && 
+    typeof aBlock.previousHash === "string" && 
+    typeof aBlock.timestamp === "number" && 
+    typeof aBlock.data === "string";
+  
+  public index: number;
+  public hash: string;
+  public previousHash: string;
+  public data: string;
+  public timestamp: number;
 
   constructor(
     index: number,
@@ -35,8 +42,6 @@ const genesisBlock: Block = new Block(0, "123412341234", "", "Hello", 123456);
 
 let blockchain: [Block] = [genesisBlock];
 
-console.log(blockchain);
-
 const getBlockchain = (): [Block] => blockchain;
 
 const getLastestBlock = (): Block => blockchain[blockchain.length - 1];
@@ -54,6 +59,16 @@ const createNewBlock = (data: string): Block => {
   return newBlock;
 };
 
-console.log(createNewBlock("hello"), createNewBlock("bye bye"));
+const isBlockValid = (candidateBlock: Block, previousBlock: Block): boolean => {
+  if (!Block.validateStructure(candidateBlock)) {
+    return false;
+  } else if (previousBlock.index + 1 !== candidateBlock.index) {
+    return false;
+  } else if (previousBlock.hash !== candidateBlock.previousHash) {
+    return false;
+  } else if () {
+
+  }
+};
 
 export {};
